de06d9e747a1ac794a70942522617629d352f090
BirjuVachhani <brvachhani@gmail.com>
2018-10-15 21:09:05 +0530

    feat: add animation details screen feat: pass vectorModel object to anim activity

---
+++ app/src/main/java/com/birjuvachhani/vectoranimations/view/activities/AnimationActivity.kt
@@ -0,0 +1,13 @@
+package com.birjuvachhani.vectoranimations.view.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.birjuvachhani.vectoranimations.R
+
+class AnimationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animation)
+    }
+}
